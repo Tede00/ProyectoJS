d1df0b27f1e0b43b0e15297ff200b9532f838259
Tomas <diaztomas6885@gmail.com>
2023-12-13 19:17:40 -0300

    tienda y storage

--- js/index.js
+++ js/index.js
@@ -25,7 +25,6 @@ Formulario.addEventListener("submit",(e)=>{
     ARRAY_CLIENTE.push(cliente);
 
     Formulario.reset();
-
 });
 
 
@@ -72,3 +71,76 @@ controlNoPago= ControlDePago(cliente.fechaPago);
 valor= Membresia(cliente);
 
 AvisoDePago(controlNoPago, cliente,valor);
+
+
+//----------------TIENDA------------------------
+
+//lista de productos disponibles en mi carrito
+
+const productos = [
+    {id:1, nombre:"Proteina",imagen:"../assets/img/1.webp", precio:13000},
+    {id:2, nombre:"Creatina",imagen:"../assets/img/2.jpg", precio:10000},
+    {id:3, nombre:"Pre-entreno",imagen:"../assets/img/3.jpeg", precio:9000},
+];
+
+//funcion que agregue porductos a un carrito
+
+function agregarACarrito(id,nombre,precio){
+    
+    const carrito= JSON.parse(localStorage.getItem('carrito')) || [];
+    carrito.push({id, nombre, precio});
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    mostrarCarrito();
+    
+}
+;
+
+function mostrarCarrito() {
+    const carrito = JSON.parse(localStorage.getItem('carrito'))|| [];
+    const lista_productos = document.getElementById('listaProductos');
+    const totalElement = document.getElementById('total');
+    let total = 0;
+
+    lista_productos.innerHTML = '';
+
+    carrito.forEach(producto => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <span>${producto.nombre}</span>
+            <span>$${producto.precio}</span>
+        `;
+        lista_productos.appendChild(listItem);
+
+        total += producto.precio;
+
+    });
+
+    totalElement.textContent = total;
+};
+//function para vaciar el carrito
+
+function vaciarCarrito(){
+    localStorage.removeItem('carrito');
+    mostrarCarrito();
+
+}
+
+//funcion para generar tarjetas de productos
+
+function generarTarjetaProductos() {
+    const productosContainer = document.getElementById('productos');
+
+    productos.forEach(producto => {
+        const productoElement = document.createElement('div' );
+        productoElement.classList.add('producto');
+        productoElement.innerHTML = `
+            <h3>${producto.nombre}</h3>
+            <p>$${producto.precio} </p>
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <button onclick="agregarACarrito(${producto.id},'${producto.nombre}', ${producto.precio})"> Agregar al carrito </button>
+        `;
+        productosContainer.appendChild(productoElement);
+    });
+};
+
+generarTarjetaProductos();
